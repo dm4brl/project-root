d7b19d722e4aaa6f1980b1f61556ea8ea2937379
dm4brl <92757328+dm4brl@users.noreply.github.com>
2023-10-25 16:52:03 -0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "gcp_project" {
+  description = "Google Cloud Project ID"
+}
+
+variable "credentials_file" {
+  description = "Path to Google Cloud service account JSON key file"
+}
+
+variable "region" {
+  description = "GCP region for the cluster"
+}
